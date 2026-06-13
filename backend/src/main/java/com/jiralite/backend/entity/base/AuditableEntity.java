954bac087d13cd47515c458b0f6ddb2bd15c11cf
ba-amanou <218820324+ba-amanou@users.noreply.github.com>
2026-06-13 23:28:55 +0200

    fix(entities): add `@CreatedBy` and `@EntityListeners` to AuditableEntity

--- backend/src/main/java/com/jiralite/backend/entity/base/AuditableEntity.java
+++ backend/src/main/java/com/jiralite/backend/entity/base/AuditableEntity.java
@@ -1,7 +1,11 @@
 package com.jiralite.backend.entity.base;
 
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import com.jiralite.backend.entity.User;
 
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -10,7 +14,9 @@ import lombok.Getter;
 
 @MappedSuperclass
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 public abstract class AuditableEntity extends TimestampedEntity {
+    @CreatedBy
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "created_by")
     private User createdBy;
